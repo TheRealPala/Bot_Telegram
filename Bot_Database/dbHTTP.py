bd7efd22e21efef2d62fc86c62b7800f7e72b98a
ale <apalano23@gmail.com>
2021-05-13 00:27:35 +0200

    Inserita la gestione degli esempi, delle traduzioni e dei campi non inseriti

--- Bot_Database/dbHTTP.py
+++ Bot_Database/dbHTTP.py
@@ -39,12 +39,23 @@ class DB:
     def getExample(self, word, lang):
         js = self.getJson(word)
         stri = ""
-        count = 1
+        count = 0
         for w in js["data"]["usable"]:
             for i in w["Examples"]:
                 if(i["LangExample"] == lang):
-                    stri += ( str(count) + ")  " + i["Example"] + "\n\n")
                     count += 1
+                    break
+        if(count == 0):
+            if(lang == "IT"):
+                stri = "Non ci sono esempi inseriti nel database!"
+            elif(lang == "EN"):
+                stri = "There are no examples available in our database!"
+        else:
+            for w in js["data"]["usable"]:
+                for i in w["Examples"]:
+                    if(i["LangExample"] == lang):
+                        stri += ( str(count) + ")  " + i["Example"] + "\n\n")
+                        count += 1
         return stri
 
     def isThereWord(self, word):
@@ -55,6 +66,82 @@ class DB:
             return 2
         return False
     
+    def getSinonimsIT(self, word):
+        js = self.getJson(word)
+        count = 0
+        stri = ""
+        for i in js["data"]["usable"]:
+            for j in i["SinonimsIT"]:
+                count += 1
+                if(count >= 0):
+                    break
+        if (count == 0):
+            stri = "La parola ricercata non ha sinonimi inseriti nel nostro database!"
+        else:
+            count = 1
+            for i in js["data"]["usable"]:
+                for j in i["SinonimsIT"]:
+                    stri += ( str(count) + ")  " + j + "\n\n")
+                    count += 1
+        return stri
+
+    def getSinonimsEN(self, word):
+        js = self.getJson(word)
+        count = 0
+        stri = ""
+        for i in js["data"]["usable"]:
+            for j in i["SinonimsEN"]:
+                count += 1
+                if(count >= 0):
+                    break
+        if (count == 0):
+            stri = "The word you are looking for has no synonyms in our database!"
+        else:
+            count = 1
+            for i in js["data"]["usable"]:
+                for j in i["SinonimsEN"]:
+                    stri += ( str(count) + ")  " + j + "\n\n")
+                    count += 1
+        return stri
+    
+    def getTranslationIT(self, word):
+        js = self.getJson(word)
+        count = 0
+        stri = ""
+        for i in js["data"]["usable"]:
+            for j in i["TranslasionsIT"]:
+                count += 1
+                if(count >= 0):
+                    break
+        if (count == 0):
+            stri = "La parola ricercata non ha traduzioni inserite nel nostro database!"
+        else:
+            count = 1
+            for i in js["data"]["usable"]:
+                for j in i["TranslasionsIT"]:
+                    stri += ( str(count) + ")  " + j + "\n\n")
+                    count += 1
+        return stri
+   
+    def getTranslationEN(self, word):
+        js = self.getJson(word)
+        count = 0
+        stri = ""
+        for i in js["data"]["usable"]:
+            for j in i["TranslasionsEN"]:
+                count += 1
+                if(count >= 0):
+                    break
+        if (count == 0):
+            stri = "The word you are looking for has no translations in our database!"
+        else:
+            count = 1
+            for i in js["data"]["usable"]:
+                for j in i["TranslasionsEN"]:
+                    stri += ( str(count) + ")  " + j + "\n\n")
+                    count += 1
+        return stri
+
     def makeMsgIT(self, word):
         stri = ""
         lang = "IT"
@@ -62,7 +149,10 @@ class DB:
         stri += "Lingua: Italiano\n"
         stri += "Descrizioni: \n" + self.getDescription(word, lang)
         stri += "Esempi: \n" + self.getExample(word, lang)
+        stri += "Sinonimi: \n" + self.getSinonimsIT(word)
+        stri += "Traduzione: \n" + self.getTranslationIT(word)
         return stri
+
     
     def makeMsgEN(self, word):
         stri = ""
@@ -71,10 +161,12 @@ class DB:
         stri += "Language: English\n"
         stri += "Descriptions: \n" + self.getDescription(word, lang)
         stri += "Examples: \n" + self.getExample(word, lang)
+        stri += "Synonyms: \n" + self.getSinonimsEN(word)
+        stri += "Translations: \n" + self.getTranslationEN(word)
         return stri
     
  #Esempio di istanza della classe
-'''
+
 if __name__ == '__main__':
     db = DB()
     word = "Virus"
@@ -92,4 +184,3 @@ if __name__ == '__main__':
         print("La parola è in fase di inserimento!")
     else:
         print("Parola non trovata! Il nostro team la inserirà il prima possibile!")
-'''
